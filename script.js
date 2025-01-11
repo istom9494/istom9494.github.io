90be5b919c32fc4849936b20563aac811502c6ee
istom9494 <1705686144@qq.com>
2025-01-11 15:42:10 +0800

    delete script.js

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-// 页面加载完成后，给教程项添加淡入动画
-window.onload = function () {
-    const tutorialItems = document.querySelectorAll('.tutorial-item');
-    tutorialItems.forEach(item => {
-        item.classList.add('fade-in');
-    });
-};
 
-// 当点击教程项时，添加放大动画
-document.addEventListener('click', function (e) {
-    if (e.target.classList.contains('tutorial-item')) {
-        e.target.classList.add('scale-up');
-    }
-});
